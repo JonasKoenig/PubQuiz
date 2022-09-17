41832f7b72b374ccba78a6556f9d04475eb980e5
JonasKoenig <jonas-koenig@gmx.net>
2022-09-17 11:21:11 +0200

    added first player input button

---
+++ game-controller/src/Components/PlayerInput/PlayerInputButton.tsx
@@ -0,0 +1,8 @@
+import { Fab } from "@mui/material";
+import "./PlayerInputButton.css"
+
+const PlayerInputButton = () => {
+    return <Fab className="PlayerInputButton">Test</Fab>
+};
+
+export default PlayerInputButton;
